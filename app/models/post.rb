6956dae7da6a57ecbba9cadbe00030493b074aab
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-12-07 18:08:47 -0600

    Fix relationship author as user

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,5 @@
 class Post < ApplicationRecord
-  # def create
-  #   # @post = Post.create(title: "Hello World", text: "This is my first post", comments_counter: 0, likes_counter: 0, user_id: 1)
-  #   validates :title, presence: true, length: { minimum: 1, maximum: 50 }
-  #   validates ${:attribute}
-  # end
+  belongs_to :author, class_name: 'User', foreign_key: :user_id
+  has_many :likes
+  has_many :comments
 end
