674697f508104717406960a2833b1dd8d42ec41b
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-12-09 17:39:20 -0600

    Add test for routes in posts and users

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe "Post", type: :request do
+  describe 'Get index route of users controller' do
+    it 'returns http success' do
+      get '/users'
+      expect(response).to have_http_status(:success)
+    end
+
+  describe 'Rendering the show page for the show routes' do
+    before :each do
+      get users_path(1)
+    end
+
+    it 'Returns success status for show routes' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'Returns or renders the show template' do
+      expect(response).to render_template(:index)
+    end
   end
+end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  it "returns http success if the user exist" do
+    get user_path(id: 1)
+    expect(response).to have_http_status(:success)
   end
+
+  it "returns http success to the list of users" do
+    get users_path(id: 1)
+    debugger
+    expect(response).to have_http_status(:success)
+  end
+
+  it "validate if the controller require a value for sent back info" do
+    get users_path(id: 1)
+    expect(response.cache_control[:private]).to eq(true)
+  end
+
 end
