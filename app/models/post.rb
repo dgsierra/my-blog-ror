6c590789c9578824c4e2281f248def76c98eae31
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-12-09 14:02:08 -0600

    Resintall rspec, fix test for model.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :user_id
   has_many :likes
   has_many :comments
+  validates :title, presence: true, length: { maximum: 100 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+  validates :name, presence: true
+  validates :bio, presence: true, length: { maximum: 500 }
 end
