1f8c20ba8640dde7567f6b8e6e5f71457ea85c22
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2022-12-10 17:26:05 -0600

    Add style and routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "users#index"
   resources :users, only: [:show, :index] do
     resources :posts, only: [:show, :index]
     end
